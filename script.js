5d31c0bc86383ccfede298345b070cfb983389c3
Crystal Korth <crystalkorth@gmail.com>
2022-04-06 23:15:57 -0500

    added tallies for button 1

--- script.js
+++ script.js
@@ -1,4 +1,13 @@
+let cashier = 0
+let c_tot = 0
+let c_num = 0
+
+
+
 cashierScoreBtn1 = document.getElementById('cashier-1')
 cashierScoreBtn1.addEventListener("click", function(){
-console.log(click)
+c_tot = c_tot + 1
+c_num = c_num + 1
+cashier = c_tot / c_num
+console.log(cashier)
 })

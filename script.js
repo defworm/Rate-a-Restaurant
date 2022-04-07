a7fbd401aa2f7691ea6e49b77835e1b43162929b
Crystal Korth <crystalkorth@gmail.com>
2022-04-06 23:28:41 -0500

    added overall btns

--- script.js
+++ script.js
@@ -2,6 +2,10 @@ let cashier = 0
 let c_tot = 0
 let c_num = 0
 
+let overall = 0
+let o_tot = 0
+let o_num = 0
+
 cashierScoreBtn1 = document.getElementById('cashier-1')
 cashierScoreBtn1.addEventListener("click", function(){
 c_tot = c_tot + 1
@@ -32,4 +36,36 @@ c_tot = c_tot + 4
 c_num = c_num + 1
 cashier = c_tot / c_num
 console.log(cashier)
+})
+
+overallScoreBtn1 = document.getElementById('overall-1')
+overallScoreBtn1.addEventListener("click", function(){
+o_tot = o_tot + 1
+o_num = o_num + 1
+overall = o_tot / o_num
+console.log(overall)
+})
+
+overallScoreBtn2 = document.getElementById('overall-2')
+overallScoreBtn2.addEventListener("click", function(){
+o_tot = o_tot + 2
+o_num = o_num + 1
+overall = o_tot / o_num
+console.log(overall)
+})
+
+overallScoreBtn3 = document.getElementById('overall-3')
+overallScoreBtn3.addEventListener("click", function(){
+o_tot = o_tot + 3
+o_num = o_num + 1
+overall = o_tot / o_num
+console.log(overall)
+})
+
+overallScoreBtn4 = document.getElementById('overall-4')
+overallScoreBtn4.addEventListener("click", function(){
+o_tot = o_tot + 4
+o_num = o_num + 1
+overall = o_tot / o_num
+console.log(overall)
 })

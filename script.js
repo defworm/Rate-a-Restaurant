036900877bed3fcde14df1692b673c61194e478d
Crystal Korth <crystalkorth@gmail.com>
2022-04-06 23:21:29 -0500

    added averages for all four cashier buttons

--- script.js
+++ script.js
@@ -2,11 +2,33 @@ let cashier = 0
 let c_tot = 0
 let c_num = 0
 
-
-
 cashierScoreBtn1 = document.getElementById('cashier-1')
 cashierScoreBtn1.addEventListener("click", function(){
 c_tot = c_tot + 1
+c_num = c_num + 1
+cashier = c_tot / c_num
+console.log(cashier)
+})
+
+cashierScoreBtn2 = document.getElementById('cashier-2')
+cashierScoreBtn2.addEventListener("click", function(){
+c_tot = c_tot + 2
+c_num = c_num + 1
+cashier = c_tot / c_num
+console.log(cashier)
+})
+
+cashierScoreBtn3 = document.getElementById('cashier-3')
+cashierScoreBtn3.addEventListener("click", function(){
+c_tot = c_tot + 3
+c_num = c_num + 1
+cashier = c_tot / c_num
+console.log(cashier)
+})
+
+cashierScoreBtn4 = document.getElementById('cashier-4')
+cashierScoreBtn4.addEventListener("click", function(){
+c_tot = c_tot + 4
 c_num = c_num + 1
 cashier = c_tot / c_num
 console.log(cashier)
